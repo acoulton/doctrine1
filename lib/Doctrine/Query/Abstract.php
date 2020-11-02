094a4f16db5ed057d50050044b4bcf4c4444dd5d
acoulton <andrew@ingenerator.com>
2020-11-02 23:13:25 +0000

    Fix array-to-string conversion on array_diff building DQL
    https://github.com/hendrismit/doctrine1/commit/1a4c8c05aa30f9ca3ddf927c267c1b787ad9b92c

--- lib/Doctrine/Query/Abstract.php
+++ lib/Doctrine/Query/Abstract.php
@@ -1149,7 +1149,19 @@ abstract class Doctrine_Query_Abstract
         $copy->free();
 
         if ($componentsBefore !== $componentsAfter) {
-            return array_diff($componentsAfter, $componentsBefore);
+            $diff = [];
+
+            foreach ($componentsAfter as $key => $val) {
+                if ( ! isset($componentsBefore[$key])) {
+                    $diff[$key] = $val;
+                } else {
+                    if (is_array($componentsBefore[$key]) && ! is_array($val)) {
+                        $diff[$key] = $val;
+                    }
+                }
+            }
+
+            return $diff;
         } else {
             return $componentsAfter;
         }
